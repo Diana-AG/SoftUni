502b41cc41a75609f887f2895671d6b5c78bf0db
Diana Georgieva <DeeDee.ag@gmail.com>
2021-05-25 22:00:00 +0530

    Multidimensional Arrays - Lab

---
+++ C# Advanced/02.Multidimensional Arrays-Lab/3.PrimaryDiagonal/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            int sum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] colElements = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = colElements[col];
+
+                    if (row == col)
+                    {
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C# Advanced/02.Multidimensional Arrays-Lab/7.PascalTriangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _7.PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+
+            long[][] triangle = new long[height][];
+
+            for (int i = 0; i < triangle.Length; i++)
+            {
+                triangle[i] = new long[i + 1];
+                triangle[i][0] = 1;
+                triangle[i][triangle[i].Length - 1] = 1;
+
+                //if (triangle[i].Length > 2)
+                //{
+                    for (int j = 1; j < triangle[i].Length - 1; j++)
+                    {
+                        triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+                    }
+
+                //}
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                Console.WriteLine(string.Join(' ', triangle[i]));
+            }
+        }
+    }
+}
