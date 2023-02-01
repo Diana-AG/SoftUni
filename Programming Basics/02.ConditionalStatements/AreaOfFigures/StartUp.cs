6b0f8204a35ff84db0ae9bde3e3b9671ce3a5370
Diana Georgieva <DeeDee.ag@gmail.com>
2023-02-01 10:19:35 +0200

    Conditional Statements - Lab

---
+++ Programming Basics/02.ConditionalStatements/AreaOfFigures/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace AreaOfFigures
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string shape = Console.ReadLine();
+            double area = 0;
+            if (shape == "square")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                area = sideA * sideA;
+            }
+            else if (shape == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                area = sideA * sideB;
+            }
+            else if (shape == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = Math.PI * radius * radius;
+            }
+            else if (shape == "triangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                area = sideA * height / 2;
+            }
+            Console.WriteLine($"{area:f3}");             
+        }
+    }
+}
---
+++ Programming Basics/02.ConditionalStatements/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Bool
+{
+using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            bool isPositive = a > 0;
+            Console.WriteLine(isPositive);
+        }
+    }
+}
