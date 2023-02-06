cb205297694551a2883ece197ece717c467646f7
Diana Georgieva <DeeDee.ag@gmail.com>
2023-02-06 17:22:17 +0200

    Conditional Statements - Exercise

---
+++ Programming Basics/PB_02.ConditionalStatements Exercise/TimePlus15 Minutes/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace TimePlus15_Minutes
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int hourAfter15min = 0;
+            int minAfter15min = 0;
+             
+            if (minutes <= 44)
+            {
+                hourAfter15min = hour;
+                minAfter15min = minutes + 15;
+            }
+            else if (minutes > 44 && hour < 23)
+            {
+                hourAfter15min = hour + 1;
+                minAfter15min = (minutes + 15) % 60;
+            }
+            else if (minutes > 44 && hour >= 23)
+            {
+                hourAfter15min = hour + 1 - 24;
+                minAfter15min = (minutes + 15) % 60;
+            }                  
+            Console.WriteLine($"{hourAfter15min}:{minAfter15min:d2}");
+
+            //minutes += hour * 60 + 15;
+            //hour = minutes / 60 % 24;
+            //minutes %= 60;
+            //Console.WriteLine($"{hour}:{minutes:d2}");
+        }
+    }
+}
