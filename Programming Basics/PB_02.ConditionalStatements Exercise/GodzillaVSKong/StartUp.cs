36e07f9db87c29d4ac4a8b846911e803818791e7
Diana Georgieva <DeeDee.ag@gmail.com>
2023-02-06 17:17:48 +0200

    Conditional Statements - Exercise

---
+++ Programming Basics/PB_02.ConditionalStatements Exercise/GodzillaVSKong/StartUp.cs
@@ -0,0 +1,51 @@
+﻿namespace GodzillaVSKong
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int walkOn = int.Parse(Console.ReadLine());
+            double priceClothesOneWalkOn = double.Parse(Console.ReadLine());
+            double decor = budget * 0.1;
+            double priceForClothes = 0.0;
+            if (walkOn > 150)
+            {
+                priceForClothes = walkOn * priceClothesOneWalkOn * 0.9;
+            }
+            else
+            {
+                priceForClothes = walkOn * priceClothesOneWalkOn;
+            }
+            double expenses = decor + priceForClothes;
+            double result = Math.Abs(budget - expenses);
+            if (budget >= expenses)
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {result:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {result:f2} leva more."); ;
+            }
+
+
+            //if (budget >= expenses)
+            //{
+            //    double moneyleft = budget - expenses;
+            //    Console.WriteLine("Action!");
+            //    Console.WriteLine($"Wingard starts filming with {moneyleft:f2} leva left.");
+            //}
+            //else
+            //{
+            //    double moneyNeeded = expenses - budget;
+            //    Console.WriteLine("Not enough money!");
+            //    Console.WriteLine($"Wingard needs {moneyNeeded:f2} leva more.");
+            //}
+
+
+        }
+    }
+}
---
+++ Programming Basics/PB_02.ConditionalStatements Exercise/WorldSwimmingRecord/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace WorldSwimmingRecord
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timeInSecForOneMeter = double.Parse(Console.ReadLine());
+
+            double score = distanceInMeters * timeInSecForOneMeter;
+            double delay = Math.Floor(distanceInMeters / 15) * 12.5;
+            double totalScore = score + delay;
+
+            double result = Math.Abs(record - totalScore);
+            if (record > totalScore)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalScore:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {result:f2} seconds slower.");
+            }
+        }
+    }
+}
