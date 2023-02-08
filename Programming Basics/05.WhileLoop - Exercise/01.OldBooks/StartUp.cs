932d243d48dbfe6f016d889d0df36f6ea635bd73
Diana Georgieva <DeeDee.ag@gmail.com>
2023-02-08 17:34:48 +0200

    While Loop - Exercise

---
+++ Programming Basics/05.WhileLoop - Exercise/01.OldBooks/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace _01.OldBooks
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string wantedBook = Console.ReadLine();
+            string currentBook = Console.ReadLine();
+            int counter = 0;
+            bool isBookFound = false;
+            
+
+            while (currentBook != "No More Books")
+            {
+                if (currentBook == wantedBook)
+                {
+                    isBookFound = true;
+                    break;
+                }
+                counter++;
+                currentBook = Console.ReadLine();
+            }
+            
+            if (isBookFound)
+            {
+                Console.WriteLine($"You checked {counter} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {counter} books.");
+            }
+        }
+    }
+}
---
+++ Programming Basics/05.WhileLoop - Exercise/03.Vacation/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace _03.Vacation
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double neededMoney = double.Parse(Console.ReadLine());
+            double ownedMoney = double.Parse(Console.ReadLine());
+            int days = 0;
+            int spendingCounter = 0;
+
+
+
+            while (ownedMoney < neededMoney && spendingCounter < 5)
+            {
+                string action = Console.ReadLine();
+                double sum = double.Parse(Console.ReadLine());
+                switch (action)
+                {
+                    case "spend":
+                        ownedMoney -= sum;
+                        spendingCounter++;
+                        if (ownedMoney < 0)
+                        {
+                            ownedMoney = 0;
+                        }
+                        break;
+
+                    case "save":
+                        ownedMoney += sum;
+                        spendingCounter = 0;
+                        break;
+                }
+                days++;
+            }
+            if (ownedMoney >= neededMoney)
+            {
+                Console.WriteLine($"You saved the money for {days} days.");
+            }
+            if (spendingCounter >= 5)
+            {
+                Console.WriteLine("You can't save the money.");
+                Console.WriteLine(days);
+            }
+        }
+    }
+}
