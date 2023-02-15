26ba6613ff1a6917af828076349aaf076820ec5e
Diana Georgieva <DeeDee.ag@gmail.com>
2023-02-15 11:42:52 +0200

    View Engine - Add unit tests

---
+++ C# Web/SoftUniServer/SUS.MvcFramework/ViewEngine/SusViewEngine.cs
@@ -0,0 +1,12 @@
+﻿namespace SUS.MvcFramework.ViewEngine
+{
+    using System;
+
+    public class SusViewEngine : IViewEngine
+    {
+        public string GetHtml(string templateCode, object viewModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
