d4d4259992d479665fd0105af44695081d001059
Diana Georgieva <DeeDee.ag@gmail.com>
2023-02-17 11:54:07 +0200

    Workshop2 - Add HttpAttributes

---
+++ C# Web/SoftUniServer/SUS.MvcFramework/BaseHttpAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace SUS.MvcFramework
+{
+    using System;
+    using System.Net.Http;
+
+    public abstract class BaseHttpAttribute : Attribute
+    {
+        public string Url { get; set; }
+
+        public abstract HttpMethod Method { get; }
+    }
+}
---
+++ C# Web/SoftUniServer/SUS.MvcFramework/HttpGetAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace SUS.MvcFramework
+{
+    using System.Net.Http;
+
+    public class HttpGetAttribute : BaseHttpAttribute
+    {
+        public HttpGetAttribute()
+        {
+        }
+        
+        public HttpGetAttribute(string url)
+        {
+            this.Url = url;
+        }
+        public override HttpMethod Method => HttpMethod.Get;
+
+        public string Url { get; }
+    }
+}
