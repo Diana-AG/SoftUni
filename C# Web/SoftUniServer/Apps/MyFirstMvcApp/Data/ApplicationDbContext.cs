2c05030486d5ada27eedfa759aa00d6571720780
Diana Georgieva <DeeDee.ag@gmail.com>
2023-02-20 12:59:27 +0200

    Workshop2 - Add DbContext, Entities and Migrations

---
+++ C# Web/SoftUniServer/Apps/MyFirstMvcApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿namespace BattleCards.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+        public ApplicationDbContext(DbContextOptions dbOptions)
+            :base(dbOptions)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=BattleCards;Integrated Security=true;");
+            }    
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserCard>().HasKey(x => new { x.UserId, x.CardId });
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Card> Cards { get; set; }
+
+        public DbSet<UserCard> UserCards { get; set; }
+    }
+}
