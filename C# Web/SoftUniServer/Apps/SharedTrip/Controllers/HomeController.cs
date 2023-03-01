b911a83633ae005d7a3f58dd7108aaa5f498e8f8
Diana Georgieva <DeeDee.ag@gmail.com>
2023-03-01 11:51:06 +0200

    Add controllers

---
+++ C# Web/SoftUniServer/Apps/SharedTrip/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿namespace SharedTrip.Controllers
+{
+    using SUS.HTTP;
+    using SUS.MvcFramework;
+
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public HttpResponse Index()
+        {
+            if (this.IsUserSignedIn())
+            {
+                return Redirect("/Trips/All");
+            }
+            else
+            {
+                return this.View();
+            }
+        }
+    }
+}
---
+++ C# Web/SoftUniServer/Apps/SharedTrip/Controllers/TripsController.cs
@@ -0,0 +1,22 @@
+﻿namespace SharedTrip.Controllers
+{
+    using SUS.HTTP;
+    using SUS.MvcFramework;
+
+    public class TripsController : Controller
+    {
+        public HttpResponse All()
+        {
+            return this.View();
+        }
+        public HttpResponse Add()
+        {
+            return this.View();
+        }
+
+        public HttpResponse Details()
+        {
+            return this.View();
+        }
+    }
+}
