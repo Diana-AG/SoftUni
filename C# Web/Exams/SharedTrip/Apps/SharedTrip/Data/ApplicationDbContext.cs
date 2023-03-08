49137c5f9502ecc1c0d61c8b5e9b63762d8c8f46
Diana Georgieva <DeeDee.ag@gmail.com>
2023-03-08 14:19:39 +0200

    SharedTrip Exam

---
+++ C# Web/Exams/SharedTrip/Apps/SharedTrip/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿namespace SharedTrip.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+        public ApplicationDbContext(DbContextOptions dbOptions)
+            : base(dbOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserTrip>().HasKey(x => new { x.TripId, x.UserId });
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=SharedTrip;Integrated Security=true;Encrypt=false");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+        
+        public DbSet<Trip> Trips { get; set; }
+        
+        public DbSet<UserTrip> UserTrips { get; set; }
+    }
+}
